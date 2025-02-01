b5f072b5a4686b7353495749bd17af0bd73a70e0
SANDEEP SINGH <consultsandeepsingh@gmail.com>
2025-02-01 16:23:13 +0530

    test change on server

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/api/message', (req, res) => {
-    res.json({ message: "Hello from Node.js backend!" });
+    res.json({ message: "Hello from Sabhikuch portal from Priyanka" });
 });
 
 const PORT = process.env.PORT || 8000;
